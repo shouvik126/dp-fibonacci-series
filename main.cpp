33d1dc835710685ed8ff7c298888990b1c094cde
shouvik126 <35471259+shouvik126@users.noreply.github.com>
2018-07-11 19:18:48 +0530

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,22 @@
+/*#include<iostream>
+using namespace std;
+int fibo(int n)
+{
+    if(n<=1)
+    {
+        return n;
+    }
+    else{
+
+    return fibo(n-2)+fibo(n-1);
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the range:=";
+    cin>>n;
+    int result=fibo(n);
+    cout<<"\n\tThe fibonacci series upto "<<n<<" ="<<result;
+    return 0;
+}*/
